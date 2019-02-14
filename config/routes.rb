9c62223d4d99ddc5b1466f5817ce4104252c0d0b
Celine Duflo <cduflo@hotmail.com>
2019-02-14 18:29:55 +0100

    MVP pour les cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'cocktails', to: 'cocktails#index', as: :cocktails
+  # get 'cocktails/new', to: 'cocktails#new', as: :cocktails_new
+  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  # post 'cocktails', to: 'cocktails#edit'
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+
 end
